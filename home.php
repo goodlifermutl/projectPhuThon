e92d51456ea100406b50756a6c009d91a18207d4
rmulNickSP <51589535+rmulNickSP@users.noreply.github.com>
2019-06-21 16:24:09 +0700

    16:23 21/6/2019

--- home.php
+++ home.php
@@ -210,17 +210,17 @@ $(document).ready(function(){
 
 function format ( d ) {
     // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
+    return '<table  cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
         '<tr>'+
             '<td>Full name:</td>'+
             '<td>'+d.name+'</td>'+
         '</tr>'+
         '<tr>'+
-            '<td>Extension number:</td>'+
-            '<td>'+d.extn+'</td>'+
+            '<td> เงิน:</td>'+
+            '<td>'+d.salary+'</td>'+
         '</tr>'+
         '<tr>'+
-            '<td>Extra info:</td>'+
+            '<td >Extra info:</td>'+
             '<td>And any further details here (images etc)...</td>'+
         '</tr>'+
     '</table>';
@@ -260,6 +260,8 @@ $(document).ready(function() {
             tr.addClass('shown');
         }
     } );
+
+   
 } );
 
 </script>
