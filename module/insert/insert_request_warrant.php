469255243ffdf85e7f5f6d9e550fab2dc0c128be
watcharakhom <58461102+watcharakhom@users.noreply.github.com>
2019-12-28 16:03:12 +0700

    123

--- module/insert/insert_request_warrant.php
+++ module/insert/insert_request_warrant.php
@@ -64,11 +64,109 @@
               </div>
               </div>
               <div class="form-group row">
-                <label class="col-form-label">ขอยื่นคำร้องขอออกหมายจับต่อศาล  ดังมีข้อความที่จะกล่าวดังต่อไปนี้ ข้อ 1. : </label>
+                <label class="col-form-label">ขอยื่นคำร้องขอออกหมายจับต่อศาล  ดังมีข้อความที่จะกล่าวดังต่อไปนี้ ข้อ 1. ด้วย : </label>
                 <div class="col">
                 <input type="text" class="form-control" id="">
                 </div>
                 </div>
+                <div class="form-group row">
+                  <label class="col-form-label">ซึ่งมีตำหนิรูปพรรณตามที่แนบมาพร้อมนี้ : </label>
+                </div>
 
+                  <div class="form-check">
+                    <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                    <label class="form-check-label" for="exampleRadios1">ได้หรือน่าจะได้กระทำความผิดอาญาร้ายแรงซึ่งมีอัตราโทษจำคุกอย่างสูงเกิน 3 ปี</label>
+                  </div>
+                  <p></p>
+                  <div class="form-check">
+                    <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                    <label class="form-check-label" for="exampleRadios1">ได้หรือน่าจะได้กระทำความผิดอาญา  และน่าจะหลบหนีหรือไปยุ่งเหยิงกับพยานหลักฐานหรือก่ออันตรายประการอื่น</label>
+                  </div>
+                  <p></p>
+                  <div class="form-group row">
+                    <label class="col-form-label">เหตุเกิดที่ : </label>
+                    <div class="col">
+                    <input type="text" class="form-control" id="">
+                    </div>
+                    </div>
+                    <div class="form-group row">
+                      <label class="col-form-label">มีพฤติการณ์กระทำความผิดที่เกี่ยวกับเหตุออกหมายจับ คือ	 : </label>
+                      <div class="col">
+                      <input type="text" class="form-control" id="">
+                      </div>
+                      </div>
+                      <div class="form-group row">
+                        <label class="col-form-label">การกระทำของผู้ต้องหาเป็นการกระทำความผิด ฐาน : </label>
+                        <div class="col">
+                        <input type="text" class="form-control" id="">
+                        </div>
+                        </div>
+                        <div class="form-group row">
+                          <label class="col-form-label">พยานเอกสาร และพยานวัตถุ : </label>
+                          <div class="col">
+                          <input type="text" class="form-control" id="">
+                          </div>
+                          </div>
+                          <div class="form-group row">
+                            <label class="col-form-label">พยานบุคคล ได้ทำการสอบสวนแล้วจำนวน  ปากที่ 1. : </label>
+                            <div class="col">
+                            <input type="text" class="form-control" id="">
+                            </div>
+                            </div>
+                            <div class="form-group row">
+                              <label class="col-form-label">ข้อ 2. ผู้ร้องประสงค์จะทำการจับกุม : </label>
+                              <div class="col">
+                              <input type="text" class="form-control" id="">
+                              </div>
+                              <label class="col-form-label">จึงขอให้ศาลออกหมายจับ : </label>
+                              <div class="col">
+                              <input type="text" class="form-control" id="">
+                              </div>
+                              <label class="col-form-label">มาดำเนินคดี</label>
+                              </div>
+                              <div class="form-group row">
+                                <label class="col-form-label">ในการยื่นคำร้องนี้ ผู้ร้องได้มอบหมายให้  : </label>
+                                <div class="col">
+                                <input type="text" class="form-control" id="">
+                                </div>
+                                <label class="col-form-label">ตำแหน่ง : </label>
+                                <div class="col">
+                                <input type="text" class="form-control" id="">
+                                </div>
+                                </div>
+                                  <div class="form-group row">
+                                  <label class="col-form-label">ผู้ร้อง </label>
+                                  <div class="form-check form-check-inline">
+                                      &nbsp;<input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                                      <label class="form-check-label" for="inlineCheckbox2">เคย</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                      <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                                      <label class="form-check-label" for="inlineCheckbox2">ไม่เคย</label>
+                                    </div>
+                                    <label class="col-form-label">ร้องขอให้ศาล </label>
+                                    <div class="col">
+                                    <input type="text" class="form-control" id="">
+                                    </div>
+                                    </div>
+                                    <div class="form-group row">
+                                    <label class="col-form-label">ออกหมายจับบุคคลดังกล่าว  โดยอาศัยเหตุแห่งการร้องขอเดียวกันนี้ หรือเหตุอื่น ๆ ( ระบุ ) </label>
+                                  </div>
+                                  <div class="col">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                    </div>
+                                    <div class="form-group row">
+                                    <label class="col-form-label">และมีคำสั่งศาล</label>
+                                    <div class="col">
+                                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                  </div>
+                                  </div>
+                                  <div class="form-group row">
+                                    <label class="col-form-label">ลงชื่อ </label>
+                                    <div class="col">
+                                    <input type="text" class="form-control" id="">
+                                    </div>
+                                    </div>
+                                    <button type="button" class="btn btn-primary btn-lg btn-block">บันทึกข้อมูล</button>
 </form>
 </div>
