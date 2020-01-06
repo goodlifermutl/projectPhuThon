0fc47d047adc3a499f84cf827407c2aa4025171a
rmulNickSP <51589535+rmulNickSP@users.noreply.github.com>
2020-01-06 17:16:03 +0700

    :(

--- home.php
+++ home.php
@@ -123,7 +123,7 @@ if($ac_chk=='4'){
 ?>
 <?php echo $com_s ?>
 <div class="sticky-top"> <!-- sticky-top -->
- <nav class="navbar navbar-expand-lg navbar-light bg-light"> <!--fixed-top-->
+ <nav class="navbar navbar-expand-lg navbar-green bg-light"> <!--fixed-top-->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
