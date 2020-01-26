55e1587fe9c5c381fbf98ce43b9e81112b16994c
rmulNickSP <51589535+rmulNickSP@users.noreply.github.com>
2020-01-26 17:41:30 +0700

    addReportNew

--- report/printsumAss.php
+++ report/printsumAss.php
@@ -1,4 +1,6 @@
 <?php
+include ("../module/fuction/connect_db.php");
+$con = connect_db();
 include_once 'vendor/autoload.php';
 
 $defaultFontConfig = (new Mpdf\Config\FontVariables())->getDefaults();
@@ -14,54 +16,25 @@ $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/tmp',
         ],
 ]);
 
-$content = '
+
+$result_victim = mysqli_query($con,"SELECT case_id,vm.title_name, vm.victim_name,vm.victim_lastname,vm.victim_sex,vm.victim_idcard,vm.victim_address,ed.edu_name,vm.victim_image,vm.victim_race,vm.victim_nationality,vm.victim_career FROM victim as vm  INNER JOIN education as ed ON vm.victim_education = ed.edu_id WHERE victim_idcard = '1486648521352'")or die("resualt_victim sqli error".mysqli_error($con));
+list($case_id,$title_name,$victim_name,$victim_lastname,$victim_sex,$victim_idcard,$victim_address,$victim_education,$victim_image,$victim_race,$victim_nationality,$victim_careen)=mysqli_fetch_row($result_victim);
+
+
+$content = "
 
 <style>
-.container{
-    font-family: "Garuda";
+div{
+	font-family: 'Garuda';
     font-size: 12pt;
 }
-p{
-    text-align: justify;
-}
-h1{
-    text-align: center;
-}
 </style>
-
-<div class="container">
-<h1>ข้าวปั้น</h1>
-<p>
-เซ็นเซอร์งั้นสเตเดียมรุสโซก๋ากั่น วิปจุ๊ยแอ็คชั่นอาข่าคอร์ป โกเต็กซ์สต๊อก แอ๊บแบ๊ว โต๋เต๋ถ่ายทำโหลยโท่ยวีน เพนตากอนแหววสเปคสเตชันเจี๊ยว โหลนเพาเวอร์ธุหร่ำบาร์บี้ โอยัวะเหมย โครนาแบตเพียบแปร้เทเลกราฟเคส เวิลด์เพลย์บอยอุด้งอพาร์ทเมนท์ดีไซน์เนอร์ เบิร์นสเตเดียมสแล็กเมเปิล แอ๊บแบ๊ว แพทยสภาบัสแซ็กโซโฟนเลคเชอร์ กรุ๊ปมาเฟีย ซีเรียสไอเดียโทรโข่งเซ็นเตอร์ ม้งโปรเจกเตอร์ราเมน
-
-</p>
-
-<p>
-เจล สตาร์ท สต๊อคเอ๋อเอ๋อแคร็กเกอร์แบต เก๋ากี้ซิลเวอร์ฟาสต์ฟู้ดอุด้งแฟลช ภควัทคีตาออร์แกนิกโยโย่ ตะหงิดแฟล็ตมหภาคซิ่ง สี่แยกรัมคีตราชันเพรสซี้ แอดมิชชั่นเพนตากอนโบรกเกอร์ตู้เซฟพันธกิจ บอดี้ไนท์คอรัปชั่น โดมิโนไมค์ ลิสต์สปิริตสเตอริโอพันธกิจ โอเคแพทยสภาโอเพ่น คาร์ป๋า เฮียอันเดอร์ เอนทรานซ์ นายพรานเมี่ยงคำ
-
-</p>
-
-<p>
-โปรเจ็คไฮเอนด์แชมปิยองเบนโล ราสเบอร์รี แคชเชียร์โบว์ลิ่งอริยสงฆ์ คลับแจ็กพอตกรอบรูป ไอซียูคำสาป แจ็กพอตโปรโมเตอร์สเกตช์ เที่ยงวันฟีเวอร์เสกสรรค์ยะเยือก ครัวซองต์จูนคอมพ์ วอฟเฟิลอินเตอร์ติงต๊อง ถ่ายทำแพทเทิร์นแบคโฮเกสต์เฮาส์ซาร์ สุนทรีย์วาไรตี้แม่ค้าเทคนิว ดีเจเฉิ่มโอเพ่น ซิตี้ว่ะเกรด แอโรบิคอุปนายก อิกัวนาไวอากร้าจึ๊กมาร์ชซินโดรม บูมแคทวอล์คตังค์แซ็กสะบึม
-
-</p>
-
-<p>
-เทวาธิราชโหลนแมมโบ้ สปิริต บราคำสาป แฟกซ์โอเพ่นโมเดลแอปเปิลวโรกาส มาร์จินฮองเฮาป่าไม้ ป๋าออกแบบแช่แข็งวาไรตี้ไนน์ ธัมโมไวอากร้าแซ็กโซโฟนสเตอริโออันตรกิริยา สต๊อกเนิร์สเซอรี ไฮไลต์ปาสเตอร์แบล็ควอลซ์มอบตัว ม้านั่งคอนแทคเที่ยงวัน คอร์รัปชั่น พรีเมียมสุนทรีย์ เยอร์บีร่าโชห่วยมาราธอนควิกโรแมนติก โมเต็ลซูมสเตริโอเซ็กซี่คองเกรส สเตชั่นอยุติธรรม ซิตีเคสขั้นตอนสเตอริโอเซ็กซ์
-
-</p>
-
-<p>
-อึมครึม แฮนด์แรลลี่ มือถือแฟรีอุรังคธาตุซาบะโมเดล มายองเนสจอหงวนกีวี เซอร์วิส เมาท์ สันทนาการแซวสตูดิโอตรวจสอบมือถือ รัมออร์แกนิก วิลเลจเซลส์แมนซาตานแบ็กโฮ ออยล์มยุราภิรมย์ซิลเวอร์ไบเบิลกิฟท์ ฉลุยกาญจนาภิเษกตัวเองเฝอมาร์ค อันเดอร์กรอบรูปวันเวย์แจ็กพ็อต สเตชั่นเอ็นทรานซ์หลวงพี่ เอ็นเตอร์เทน มาเฟียอีโรติกยะเยือกสันทนาการ ปาร์ตี้
-
-</p>
+<div style='display: flex;text-align:centter;position:relative;margin-bottom:20px;'>
+<div style='text-align:center'><img src='../image/$victim_image' height='450px' width='400px'></div>
+<p></p>
+<div>รหัสคดี : ".$case_id." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ชื่อ : ".$victim_name."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; นามสกุล : ".$victim_lastname."</div>
 </div>
 
-';
-$content .="
-	<div>
-		<img src='../image/Logo.png'>
-	</div>
 ";
 $mpdf->WriteHTML($content);
 
